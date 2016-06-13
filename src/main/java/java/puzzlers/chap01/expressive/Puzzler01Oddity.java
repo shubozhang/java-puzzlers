f1866c3c34c06a6b81c485bdfd3c57038ee17fc3
Zhang <shubo.zhang@multiplan.com>
2016-06-13 17:22:35 -0400

    add chapter01

---
+++ src/main/java/java/puzzlers/chap01/expressive/Puzzler01Oddity.java
@@ -0,0 +1,12 @@
+package java.puzzlers.chap01.expressive;
+
+/**
+ * Determine whether an integer is an odd number
+ *
+ * Java reminder operator: (a/b)*b + (a%b) == a
+ */
+public class Puzzler01Oddity {
+
+
+
+}
