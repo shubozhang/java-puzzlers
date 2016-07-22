c8271def8a5a224678bb6f20785cf0babe9cf8ac
Bryan <bryanshubo@gmail.com>
2016-07-21 23:00:38 -0400

    chap02

--- src/main/java/com/puzzlers/chap02/character/Puzzler11LastLaugh.java
+++ src/main/java/com/puzzlers/chap02/character/Puzzler11LastLaugh.java
@@ -1,18 +1,36 @@
 package com.puzzlers.chap02.character;
 
 /**
- * Created by shubo.zhang on 7/21/2016.
+ * The + operator performs string concatenation if and only if at least one of its operands is of type String;
+ * Otherwise, the type of each of the operands of the + operator must be a type that is convertible
+ * (§5.1.8)to a primitive numeric type, or a compile-time error occurs.
  */
 public class Puzzler11LastLaugh {
 
     public static void main(String[] args) {
+
         System.out.println("H" + "a");
-        System.out.println('H' + 'a');
+        System.out.println('H' + 'a'); // char will be converted to int (a primitive numeric type)
 
-        System.out.println((int)'H');
-        System.out.println((int)Character.MAX_VALUE);
+        test1();
+        test2();
+        test3();
+    }
 
-        char test = 'H' + 'a';
-        System.out.println(test);
+    //This works, but it's ugly
+    private static void test1() {
+        StringBuffer sb = new StringBuffer();
+        sb.append('H');
+        sb.append('a');
+        System.out.println(sb);
+    }
+
+    private static void test2() {
+        System.out.println("" + 'H' + 'a');
+        System.out.printf("%c%c\n", 'H', 'a');
+    }
+
+    private static void test3() {
+        System.out.println("2 + 2 = " + 2 + 2);
     }
 }
---
+++ src/main/java/com/puzzlers/chap02/character/Puzzler12ABC.java
@@ -0,0 +1,27 @@
+package com.puzzlers.chap02.character;
+
+
+/*
+* string conversion:
+* If the reference is null, it is converted to the string "null".
+* Otherwise, the conversion is performed as if by an invocation of the toString method of the referenced
+* object with no arguments; but if the result of invoking the toString
+* method is null, then the string "null" is used instead
+* */
+public class Puzzler12ABC {
+
+    public static void main(String[] args) {
+        String letters = "ABC";
+        char[] numbers = { '1', '2', '3' };
+        // Equivalent
+        System.out.println(letters + " easy as " + numbers);
+        System.out.println(letters + " easy as " + numbers.toString());
+
+        // Equivalent
+        System.out.println(letters + " easy as " + 123);
+        System.out.println(letters + " easy as " + new Integer(123).toString());
+
+        // To fix the issue
+        System.out.println(letters + " easy as " + String.valueOf(numbers));
+    }
+}
