21e4f0cd71d5e144ae3b08dbedcb8a54bb4c4906
K26511 <shubo.zhang@finra.org>
2016-11-28 17:47:09 -0500

    add puzzle 44

--- src/main/java/com/puzzlers/chap04/exceptionalpuzzlers/Puzzle44CuttingClass.java
+++ src/main/java/com/puzzlers/chap04/exceptionalpuzzlers/Puzzle44CuttingClass.java
@@ -1,7 +1,62 @@
 package com.puzzlers.chap04.exceptionalpuzzlers;
 
 /**
- * Created by k26511 on 11/18/2016.
+ * do not depend on catching NoClassDefFoundError. The language
+ * specification carefully describes when class initialization occurs
+ * [JLS 12.4.1], but class loading is far less predictable. More generally, it is rarely
+ * appropriate to catch Error or its subclasses. These exceptions are reserved for
+ * failures from which recovery is not feasible.
  */
 public class Puzzle44CuttingClass {
+
+    public static void main(String[] args) {
+        test01();
+
+        test02();
+
+        test03();
+    }
+
+
+    /*
+    * throws an uncaught NoClassDefFoundError
+    * */
+    private static void test01() {
+
+        try {
+            Puzzle44Helper m = new Puzzle44Helper();
+        } catch (java.lang.NoClassDefFoundError ex) {
+            System.out.println("Got it!");
+        }
+    }
+
+
+    /*
+    * prints Got it!
+    * */
+    private static void test02() {
+        Puzzle44Helper m;
+        try {
+            m = new Puzzle44Helper();
+        } catch (java.lang.NoClassDefFoundError ex) {
+            System.out.println("Got it!");
+        }
+    }
+
+
+    /*
+    * Best implementation
+    * */
+
+    private static void test03() {
+        try {
+            Object m = Class.forName("Puzzle44Helper").newInstance();
+        } catch (ClassNotFoundException ex) {
+            System.err.println("Got it!");
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/com/puzzlers/chap04/exceptionalpuzzlers/Puzzle44Helper.java
@@ -0,0 +1,10 @@
+package com.puzzlers.chap04.exceptionalpuzzlers;
+
+/**
+ * Created by K26511 on 11/28/2016.
+ */
+public class Puzzle44Helper {
+
+    public Puzzle44Helper() {
+    }
+}
